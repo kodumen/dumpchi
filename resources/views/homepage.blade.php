8b3a202670e08da239f5f4924eeb8ca3d206794b
kodumen <r.b.daitan@gmail.com>
2015-04-03 19:56:19 +0800

    Added background image.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Show My Window</title>
+    <link href="{{ asset('css/stylesheet.css') }}" type="text/css" rel="stylesheet">
 </head>
 <body>
-
+<div id="window" style="background-image: url('{{ asset($photo) }}');"></div>
 </body>
 </html>
