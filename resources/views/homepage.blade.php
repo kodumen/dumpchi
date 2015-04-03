9db9f45fe0aab7448babc5ba73d9ae50e0a671a9
kodumen <r.b.daitan@gmail.com>
2015-04-03 22:18:41 +0800

    Updated homepage.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Show My Window</title>
-    <link href="{{ asset('css/stylesheet.css') }}" type="text/css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div id="window" style="background-image: url('{{ asset($photo) }}');"></div>
