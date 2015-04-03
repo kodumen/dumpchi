aab497c2668f303a9ec979822c4e11266cbdd0b9
kodumen <r.b.daitan@gmail.com>
2015-04-03 15:04:56 +0800

    Updated form.

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,10 +5,13 @@
     <title>Upload</title>
 </head>
 <body>
-<form action="{{ action('UploadController@postPhoto') }}" method="post">
+<form action="{{ action('UploadController@postPhoto') }}" method="post" enctype="multipart/form-data">
     <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
     <input type="file" name="photo" id="photo" accept="image/jpeg, image/png"/>
     <button type="submit">Upload</button>
 </form>
+@foreach($errors as $error)
+    {{ $error }}
+@endforeach
 </body>
 </html>
