9c8b315c60b645fc081d63005293f0b73600388d
kodumen <r.b.daitan@gmail.com>
2015-04-03 16:36:56 +0800

    Added function to get photo filename.

--- app/Photo.php
+++ app/Photo.php
@@ -5,5 +5,13 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model {
 
 	protected $table = 'photos';
-    
+
+    /**
+     * Return the filename of the photo uploaded.
+     * @return string
+     */
+    public function getFilename() {
+        $filename = sprintf('%d.%s', $this->id, $this->file_extension);
+        return $filename;
+    }
 }
