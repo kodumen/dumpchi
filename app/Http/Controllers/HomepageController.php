935cb6c8ff89be20148a2870fcfbf2c7ceb06686
kodumen <r.b.daitan@gmail.com>
2015-04-03 19:17:47 +0800

    Updated homepage controller to get random image.

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -4,11 +4,13 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use App\Photo;
 
 class HomepageController extends Controller {
 
 	public function getIndex() {
-        return view('homepage');
+        $photo = Photo::all()[rand(0, Photo::all()->count() - 1)];
+        return view('homepage')->with('photo', 'img/'.$photo->getFilename());
     }
 
 }
