4358dae9369b00b198705533892cb25abe9aa5a3
kodumen <r.b.daitan@gmail.com>
2015-04-04 15:50:59 +0800

    Changed title.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" style="background-image: url('{{ asset($photo) }}');">
 <head>
     <meta charset="UTF-8">
-    <title>Show My Window</title>
+    <title>outside the window</title>
     <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
 </head>
