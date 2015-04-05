a1902a919c19d98b5a158798eef649f6832b1445
kodumen <r.b.daitan@gmail.com>
2015-04-05 15:14:13 +0800

    Removed function from Photo model.

--- app/Photo.php
+++ app/Photo.php
@@ -5,13 +5,5 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model {
 
 	protected $table = 'photos';
-
-    /**
-     * Return the filename of the photo uploaded.
-     * @return string
-     */
-    public function getFilename() {
-        $filename = sprintf('%d.%s', $this->id, $this->file_extension);
-        return $filename;
-    }
+    
 }
